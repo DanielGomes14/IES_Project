5b21dddb1970bbad2e4b38e359b128a4f7fb5cdd
Mário Francisco Costa Silva <mariosilva@ua.pt>
2020-11-29 16:21:18 +0000

    login final adjustments

--- projWebsite/src/views/Login.js
+++ projWebsite/src/views/Login.js
@@ -3,29 +3,25 @@ import { Container, Row, Col, Card, CardHeader, CardBody, Button } from "shards-
 import LoginPageTitle from "../components/common/LoginPageTitle";
 import Forms from "../components/components-overview/Forms";
 import FormValidation from "../components/components-overview/FormValidation";
-import CompleteFormExample from "../components/components-overview/CompleteFormExample";
+import LoginForm from "../components/components-overview/LoginForm";
 
 
 
 const Login = () => (
-    <Container fluid className="main-content-container px-4">
+  <Container fluid className="main-content-container px-4 py-5 d-flex justify-content-center">
 
-      <Row className="mb-0 page-header py-4 text-sm-center">
-        <LoginPageTitle sm="4" title="Log In" className="text-sm-center" />
-      </Row> 
+    <Row className="mb-0 col d-flex justify-content-center">
+        <Col sm="6">
+            <Card>
+                <CardHeader className="border-bottom">
+                    <LoginPageTitle sm="4" title="Login" className="text-sm-center" />
+                </CardHeader>
+                <LoginForm />
+            </Card>
+        </Col>
+    </Row>
 
-      <Row className="mb-0 col d-flex justify-content-center">
-        <Card small >
-          <CardHeader className="border-bottom">
-            <h6 className="m-0">Form Example</h6>
-          </CardHeader>
-          <CompleteFormExample />
-        </Card>
-      </Row>
-
-
-
-    </Container>
+  </Container>
   );
   
   export default Login;
