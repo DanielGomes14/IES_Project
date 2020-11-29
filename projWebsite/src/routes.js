ed7239d914b2ec72e47bef22b8f5936aa219081f
BrunosBastos <Brunosb@ua.pt>
2020-11-29 16:51:41 +0000

    automation tab

--- projWebsite/src/routes.js
+++ projWebsite/src/routes.js
@@ -12,6 +12,7 @@ import Errors from "./views/Errors";
 import ComponentsOverview from "./views/ComponentsOverview";
 import Tables from "./views/Tables";
 import BlogPosts from "./views/BlogPosts";
+import Automation from "./views/Automation";
 
 export default [
   {
@@ -54,5 +55,10 @@ export default [
     path: "/blog-posts",
     layout: DefaultLayout,
     component: BlogPosts
+  },
+  {
+    path: "/automation",
+    layout: DefaultLayout,
+    component : Automation
   }
 ];
---
+++ projWebsite/src/views/Automation.js
@@ -0,0 +1,72 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import {
+    Container, Row, Col, Card, CardBody
+} from "shards-react";
+
+import ConfigProfile from "../components/automation/ConfigProfile";
+import ConfigDevice from "../components/automation/ConfigDevice";
+
+const automation = ({ profiles,devices }) => (
+    <Container fluid className="main-content-container px-4">
+        <Container >
+            <Row noGutters className="page-header py-2 ">
+                {profiles.map((profile, idx) => (
+                    <Col className="col-lg mb-4 px-2" key={idx} {...profile.attrs}>
+                        <ConfigProfile
+                            name={profile.name} />
+                    </Col>
+                ))}
+                <Col className="col-lg mb-4 px-2">
+                    <Card small>
+                        <CardBody>
+                            <div style={{ textAlign: "center" }}>
+                                <h6>+</h6>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+
+        </Container>
+        <Container>
+            
+            {devices.map((device,idx) => (
+            <Row className="py-2 px-4">
+                <ConfigDevice name={device.name} division={device.division} />
+            </Row>
+            ))}
+        </Container>
+    </Container>
+);
+
+automation.propTypes = {
+    profiles: PropTypes.array,
+    devices : PropTypes.array
+};
+
+automation.defaultProps = {
+    profiles: [{
+        name: "Default"
+    },
+    {
+        name: "Economics"
+    },
+    {
+        name: "Custom"
+    }
+    ],
+    devices: [{
+        name : "Air conditioner", 
+        division : "Kitchen"
+    },
+    {
+        name : "Lights",
+        division: "Bedroom"
+    }
+    ]
+};
+
+export default automation;
