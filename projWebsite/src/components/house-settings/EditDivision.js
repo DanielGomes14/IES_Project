e20824d6779f46b875d57ae675757064b7f117e9
Daniel Gomes <dagomes@ua.pt>
2020-11-30 16:12:03 +0000

    finished feature

--- projWebsite/src/components/house-settings/EditDivision.js
+++ projWebsite/src/components/house-settings/EditDivision.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-    Button,
 Card,
 CardHeader,
 ListGroup,
@@ -14,7 +13,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
+import MaxWidthDialog from "./DialogDivision"
+import { Button } from "@material-ui/core";
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -43,11 +43,11 @@ class EditDivision extends React.Component{
     super();
     this.OPTIONS = [{ id: 0 , name : "Daniel",checked:true }, {id: 1 , name : "Leandro",checked:true },{ id : 2 , name : "Chico", checked:true }, 
     {id : 3 , name : "Bruno" , checked:true} ];
-    this.OPTIONS2 = [{id: 0 , name : "Leandro",checked:true },{ id : 1 , name : "Chico", checked:true },  {id : 2 , name : "Bruno" , checked:true} ];
+    this.OPTIONS2 = [{id: 0 , name : "Leandro",checked:true },{ id : 1 , name : "Chico", checked:true },  {id : 2 , name : "Bruno" , checked:true}, { id: 3 , name : "Daniel",checked:false }];
     this.rows = [
         this.createData('Kitchen', this.OPTIONS),
         this.createData('Living Room', this.OPTIONS2),
-        this.createData('Bedroom1', this.OPTIONS,),
+        this.createData('Bedroom1', this.OPTIONS),
         this.createData('Bedroom2', this.OPTIONS2),
     ];
     this.state = { checked: false, title: "Edit Current Divisions' Permissions", OPTIONS : this.OPTIONS };
@@ -55,6 +55,15 @@ class EditDivision extends React.Component{
 }
     createData(name, people, fat, carbs, protein) {
     return { name, people, fat, carbs, protein };
+  }
+    getNumChecked(row){
+      let counter=0;
+      for(let i = 0 ; i< row.people.length;i++){
+        if (row.people[i].checked){
+          counter++;}
+
+    }
+    return counter
   }
   /*
   handleChange() {
@@ -79,8 +88,8 @@ return  (
         <TableHead>
           <TableRow>
             <StyledTableCell>Name of Division</StyledTableCell>
-            <StyledTableCell>People</StyledTableCell>
-            <StyledTableCell >Number of Members&nbsp;</StyledTableCell>
+            <StyledTableCell>People with Permission</StyledTableCell>
+            <StyledTableCell >Number of Members with Permission&nbsp;</StyledTableCell>
             <StyledTableCell>Edit Division&nbsp;</StyledTableCell>
           </TableRow>
         </TableHead>
@@ -94,17 +103,18 @@ return  (
                 <ul>
                     {
                     row.people.map((person) => {
-                        return (
+                      
+                        return person.checked ? (
                         <li key={person.name}>
                             { person.name }
                         </li>
-                        );
+                        ): null;
                     })
                     }
                 </ul>
       </StyledTableCell>
-                <StyledTableCell component="th"  scope="col" >{row.people.length}</StyledTableCell>
-              <StyledTableCell component="th" scope="col"><Button type="button" >Edit</Button></StyledTableCell>
+                <StyledTableCell component="th"  scope="col" >{this.getNumChecked(row)}</StyledTableCell>
+              <StyledTableCell  scope="col">< MaxWidthDialog content={row} /> <Button variant="outlined"  color="secondary">Remove</Button></StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
