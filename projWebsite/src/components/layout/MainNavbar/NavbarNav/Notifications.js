5919ac65779b30e984a61cb04c00234f23de9839
Mário Francisco Costa Silva <mariosilva@ua.pt>
2020-12-01 15:56:48 +0000

    notifications bar changed

--- projWebsite/src/components/layout/MainNavbar/NavbarNav/Notifications.js
+++ projWebsite/src/components/layout/MainNavbar/NavbarNav/Notifications.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavItem, NavLink, Badge, Collapse, DropdownItem } from "shards-react";
+import { FaTemperatureHigh } from "react-icons/fa";
 
 export default class Notifications extends React.Component {
   constructor(props) {
@@ -43,24 +44,22 @@ export default class Notifications extends React.Component {
               </div>
             </div>
             <div className="notification__content">
-              <span className="notification__category">Device Log</span>
+              <span className="notification__category">Device Log <span className="text-black text-semibold">{new Date().toLocaleString()}</span></span>
               <p>
-                Coffe Machine turned <span className="text-success text-semibold">ON</span> at {' '}
-                <span className="text-black text-semibold">{new Date().toLocaleString()}</span>.
+                Coffe Machine turned <span className="text-success text-semibold">ON</span>.
               </p>
             </div>
           </DropdownItem>
           <DropdownItem>
-            <div className="notification__icon-wrapper">
+            <div className="notification__icon-wrapper" >
               <div className="notification__icon">
-                <i className="material-icons">&#xE8D1;</i>
+                <i className="material-icons">&#xE6E1;</i>
               </div>
             </div>
             <div className="notification__content">
-              <span className="notification__category">Temperature</span>
+              <span className="notification__category">Temperature <span className="text-black text-semibold">{new Date().toLocaleString()}</span></span>
               <p>
-                Living Room reached the goal temperature of <span className="text-danger text-semibold">5.52%</span>. It
-                could have been worse!
+              <span className="text-black text-semibold">Living Room</span> reached the goal temperature of <span className="text-success text-semibold">28°C</span>.
               </p>
             </div>
           </DropdownItem>
