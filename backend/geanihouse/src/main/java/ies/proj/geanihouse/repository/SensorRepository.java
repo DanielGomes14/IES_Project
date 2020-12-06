8a7043ad76938c2de00d07b8349b61c626b9b681
BrunosBastos <Brunosb@ua.pt>
2020-12-06 22:50:08 +0000

    Added Sensor Model and Repository

---
+++ backend/geanihouse/src/main/java/ies/proj/geanihouse/repository/SensorRepository.java
@@ -0,0 +1,12 @@
+package ies.proj.geanihouse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ies.proj.geanihouse.model.Sensor;
+
+import java.util.List;
+
+public interface SensorRepository extends JpaRepository<Sensor, Long>{
+    List<Sensor> findById(long id);
+}
