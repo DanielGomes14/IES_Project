fb36880fc3711147db05607f83b66f2430f8cdfa
Daniel Gomes <dagomes@ua.pt>
2020-12-07 18:41:48 +0000

    Started communications with the Broker

--- backend/geanihouse/src/main/java/ies/proj/geanihouse/GeanihouseApplication.java
+++ backend/geanihouse/src/main/java/ies/proj/geanihouse/GeanihouseApplication.java
@@ -2,7 +2,10 @@ package ies.proj.geanihouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class GeanihouseApplication {
 
