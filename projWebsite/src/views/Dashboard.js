fd9696378297bf74b7ca50ec05531aa455bbbb87
Leandro Silva <leandrosilva12@ua.pt>
2020-12-07 15:41:43 +0000

    fetch

--- projWebsite/src/views/Dashboard.js
+++ projWebsite/src/views/Dashboard.js
@@ -8,54 +8,62 @@ import DeviceCard from "./../components/dashboard/DeviceCard";
 import DivisionService from "./../services/DivisionService";
 
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      divisions: []
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false,
+			divisions: []
+		}
+  	}
 
-  componentDidMount(){
-    DivisionService.getDivisions().then((response) => {
-      this.setState({ divisions : response.data })
-    });
-  }
+	componentDidMount() {
+		this.setState({ loading: true });
+		DivisionService.getDivisions()
+			.then(data => { this.setState({ 
+					loading: false,
+					divisions : data 
+				}) 
+			});
+	}
   
-  render(){
-    return(
-      <Container fluid className="main-content-container px-4">
-        {/* Page Header */}
-        <Row noGutters className="page-header py-4">
-          <PageTitle title="Dashboard" subtitle="" className="text-sm-left mb-3" />
-        </Row>
+	render() {
+		return (
+			<Container fluid className="main-content-container px-4">
+				{/* Page Header */}
+				<Row noGutters className="page-header py-4">
+					<PageTitle title="Dashboard" subtitle="" className="text-sm-left mb-3" />
+				</Row>
 
-          {this.state.divisions.map((div) => {
-              return (
-                <DeviceGroup division={div} />
-              )
-          })}
-        
-        {/* <DeviceGroup division="Kitchen">
-          <DeviceCard title="Lights" progress={95} type="light" />
-        </DeviceGroup>
+				{this.state.loading 
+					? this.state.divisions.map(div => {
+						return (
+							<DeviceGroup division={div} />
+						)
+					})
+					: "LOADING..."
+				}
 
-        <DeviceGroup division="Living Room">
-          <DeviceCard title="Air Conditioning" progress={95} type="temperature" />
-        </DeviceGroup>
+				{/* <DeviceGroup division="Kitchen">
+				<DeviceCard title="Lights" progress={95} type="light" />
+				</DeviceGroup>
 
-        <DeviceGroup division="Bedroom #1">
-          <DeviceCard title="Zumifier" progress={10} type="humidity" />
-          <DeviceCard title="Radiator" progress={35} type="temperature" />
-        </DeviceGroup>
+				<DeviceGroup division="Living Room">
+				<DeviceCard title="Air Conditioning" progress={95} type="temperature" />
+				</DeviceGroup>
 
-        <DeviceGroup division="Bedroom #2">
-          <DeviceCard title="Zumifier" progress={55} type="humidity" />
-          <DeviceCard title="Radiator" progress={40} type="temperature" />
-          <DeviceCard title="Presence Light" progress={5} type="light" />
-        </DeviceGroup> */}
-      </Container>
-    )
-  }
+				<DeviceGroup division="Bedroom #1">
+				<DeviceCard title="Zumifier" progress={10} type="humidity" />
+				<DeviceCard title="Radiator" progress={35} type="temperature" />
+				</DeviceGroup>
+
+				<DeviceGroup division="Bedroom #2">
+				<DeviceCard title="Zumifier" progress={55} type="humidity" />
+				<DeviceCard title="Radiator" progress={40} type="temperature" />
+				<DeviceCard title="Presence Light" progress={5} type="light" />
+				</DeviceGroup> */}
+			</Container>
+		)
+	}
 }
 
 export default Dashboard;
