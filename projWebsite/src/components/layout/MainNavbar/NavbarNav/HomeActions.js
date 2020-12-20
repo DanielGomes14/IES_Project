46c45279ae3cd36b3c66695a4cef6e7582792c0f
BrunosBastos <Brunosb@ua.pt>
2020-12-20 19:46:49 +0000

    load houses and current house

--- projWebsite/src/components/layout/MainNavbar/NavbarNav/HomeActions.js
+++ projWebsite/src/components/layout/MainNavbar/NavbarNav/HomeActions.js
@@ -12,6 +12,8 @@ import {
   Button
 } from "shards-react";
 
+import HomeService from '../../../../services/HomeService';
+
 export default class HomeActions extends React.Component {
   constructor(props) {
     super(props);
@@ -19,9 +21,14 @@ export default class HomeActions extends React.Component {
     this.state = {
       visible: false
     };
-
+    this.homes = [];
+    this.current_home={"id":0,"name":"Home"};
+    HomeService.getHomesDropdown().then(data=>{this.homes=data});
+    HomeService.getHomeById().then(data=>{this.current_home=data});
+    
     this.toggleHomeActions = this.toggleHomeActions.bind(this);
   }
+  
 
   toggleHomeActions() {
     this.setState({
@@ -29,27 +36,25 @@ export default class HomeActions extends React.Component {
     });
   }
 
+  changeHome(){
+    console.log("Changing Home");
+  }
+
   render() {
     return (
         <NavItem className="border-right" tag={Dropdown} caret toggle={this.toggleHomeActions}>
             <DropdownToggle caret tag={NavLink} className="text-nowrap px-3" style={{paddingTop: "20px"}}>
-                {" "}
-                Home 1
+                {this.current_home.name}
             </DropdownToggle>
             <Collapse tag={DropdownMenu} right small open={this.state.visible}>
-                <DropdownItem tag={Link} to="#">
-                    <Badge pill theme="danger" className="mr-2" style={{lineHeight: "4px"}}> 2 </Badge>
-                    Home 1
-                </DropdownItem>
-                <DropdownItem tag={Link} to="#">
-                    <Badge pill theme="light" className="mr-2 text-white" style={{lineHeight: "4px"}}> 0 </Badge>
-                    Home 2
-                </DropdownItem>
-                <DropdownItem tag={Link} to="#">
-                    <Badge pill theme="danger" className="mr-2" style={{lineHeight: "4px"}}> 1 </Badge>
-                    Home 3
-                </DropdownItem>
-                <DropdownItem tag={Link} to="/newhouse">
+                {this.homes.map((home, index) => (
+                  <DropdownItem key={index} tag={Link}  to="#">
+                    <Badge pill theme="danger" className="mr-2"  style={{lineHeight: "4px"}}> 2 </Badge>
+                    <span onClick={this.changeHome()}>{home.name}</span>
+                  </DropdownItem>
+                ))}
+                
+                <DropdownItem  tag={Link} to="/newhouse">
                     <Button theme="primary" outline={true} className="w-100">Add Home</Button>
                 </DropdownItem>
             </Collapse>
