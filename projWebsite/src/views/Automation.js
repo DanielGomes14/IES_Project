ecc2175a70fd10ee24b6f4b8ec8937908cb9846e
Leandro Silva <leandrosilva12@ua.pt>
2021-01-01 19:43:51 +0000

    Automation to class

--- projWebsite/src/views/Automation.js
+++ projWebsite/src/views/Automation.js
@@ -57,130 +57,116 @@ function a11yProps(index) {
     };
 }
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        width: '100%',
-        backgroundColor: theme.palette.background.paper,
-    },
-}));
 
 
+class Automation extends React.Component {
 
+    constructor(props) {
+		super(props);
+		this.state = {
+            divisions: props.divisions,
+            value: 0
+        }
+        this.classes = this.useStyles;
+		// this.handleChange = this.handleChange.bind(this);
+    }
 
-
-function Automation({ divisions }) {
-
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
+    handleChange = (event, newValue) => {
+        this.setState({value: newValue});
     };
 
+    useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+            width: '100%',
+            backgroundColor: theme.palette.background.paper,
+        },
+    }));
 
-    return (
-        <Container fluid className="main-content-container px-4">
-            <Row noGutters className="page-header py-4">
-                <PageTitle sm="4" title="Automation" subtitle="Dashboard" className="text-sm-left" />
-            </Row>
+    render() {
+        return (
+            <Container fluid className="main-content-container px-4">
+                <Row noGutters className="page-header py-4">
+                    <PageTitle sm="4" title="Automation" subtitle="Dashboard" className="text-sm-left" />
+                </Row>
 
-            <Container >
-                <div className={classes.root}>
-                    <AppBar position="static" color="default">
-                        <Tabs
-                            value={value}
-                            onChange={handleChange}
-                            indicatorColor="primary"
-                            textColor="primary"
-                            variant="scrollable"
-                            scrollButtons="auto"
-                            aria-label="scrollable auto tabs example">
+                <Container >
+                    <div className={this.classes.root}>
+                        <AppBar position="static" color="default">
+                            <Tabs
+                                value={this.state.value}
+                                onChange={this.handleChange}
+                                indicatorColor="primary"
+                                textColor="primary"
+                                variant="scrollable"
+                                scrollButtons="auto"
+                                aria-label="scrollable auto tabs example">
 
 
-                            {divisions.map((division, index) => (
-                                <Tab key={index} label={division.name} {...a11yProps(index)} />
-                            ))}
+                                {this.state.divisions.map((division, index) => (
+                                    <Tab key={index} label={division.name} {...a11yProps(index)} />
+                                ))}
 
-                        </Tabs>
-                    </AppBar>
-                    {/*
-                    {profiles.map((profile, idx_prof) => (
-                        //needs a container inside of the map to work
-                        // TODO: make it pretty
-                        <TabPanel value={value} index={idx_prof}>
-                            <Button className="float-right mx-2">Edit</Button>
-                            <Button className="float-right mx-2">Duplicate</Button>
-                            <Button className="float-right mx-2">Delete</Button>
-                            <div className="clearfix"></div>
-                            {profile.devices.map((device, idx_dev) => (
+                            </Tabs>
+                        </AppBar>
+                       
+                        {this.state.divisions.map((division, index) => (
+                            <TabPanel key={index} value={this.state.value} index={index}>
+                                <Button className="float-right mx-2 mb-3">Set Default</Button>
+                                <Button className="float-right mx-2 mb-3">Set Economics</Button>
+                                <a href="/config-device"><Button className="float-right mx-2 mb-3">Custom Edit</Button></a>
+                                <div className="clearfix"></div>
 
-                                <Row className="py-2 px-4">
+                                <Row>
+                                    {division.configurations.map((configuration,index) => (
 
-                                    <ConfigDevice name={device.name} division={device.division} type={device.type} isOn={device.isOn} />
+                                        <Col key={index} lg="4">
+                                            <h4>{configuration.type}</h4>
+                                            <TypeSlider type={configuration.type} min_value={configuration.min_value}
+                                            max_value={configuration.max_value} />
+                                        </Col>
 
+
+                                    ))}
                                 </Row>
+                                
+                                <Row>
 
-                            ))}
-                        </TabPanel>
-                    ))}
-                    */}
-                    {divisions.map((division, index) => (
-                        <TabPanel key={index} value={value} index={index}>
-                            <Button className="float-right mx-2 mb-3">Set Default</Button>
-                            <Button className="float-right mx-2 mb-3">Set Economics</Button>
-                            <a href="/config-device"><Button className="float-right mx-2 mb-3">Custom Edit</Button></a>
-                            <div className="clearfix"></div>
-
-                            <Row>
-                                {division.configurations.map((configuration,index) => (
-
-                                    <Col key={index} lg="4">
-                                        <h4>{configuration.type}</h4>
-                                        <TypeSlider type={configuration.type} min_value={configuration.min_value}
-                                        max_value={configuration.max_value} />
-                                    </Col>
-
-
-                                ))}
-                            </Row>
-                            
-                            <Row>
-
-                                {division.devices.map((device, index) => (
-                                    <Col key={index} lg="6" className="py-3">
-                                        <Card className="">
-                                            <CardBody>
-                                                <h3>
-                                                    {device.name}
-                                                </h3>
-                                                <Row>
-                                                    <Col lg="7">
-                                                    <FormCheckbox toggle checked={device.state} > Off/On </FormCheckbox>
-                                                    </Col>
-                                                    <Col lg="5">
-                                                    <h6 style={{textAlign:"right"}}>{device.type}</h6>
-                                                    </Col>
-                                                </Row>                                                    
-                                                {device.configurations.map((configuration,index)=> (
-                                                    <ConfigDevice key={index} value={configuration.value} start={configuration.start} end={configuration.end}    />
-                                                ))}
+                                    {division.devices.map((device, index) => (
+                                        <Col key={index} lg="6" className="py-3">
+                                            <Card className="">
+                                                <CardBody>
+                                                    <h3>
+                                                        {device.name}
+                                                    </h3>
+                                                    <Row>
+                                                        <Col lg="7">
+                                                        <FormCheckbox toggle checked={device.state} > Off/On </FormCheckbox>
+                                                        </Col>
+                                                        <Col lg="5">
+                                                        <h6 style={{textAlign:"right"}}>{device.type}</h6>
+                                                        </Col>
+                                                    </Row>                                                    
+                                                    {device.configurations.map((configuration,index)=> (
+                                                        <ConfigDevice key={index} value={configuration.value} start={configuration.start} end={configuration.end}    />
+                                                    ))}
 
 
-                                            </CardBody>
-                                        </Card>
-                                    </Col>
-                                ))}
-                            </Row>
-                        </TabPanel>
-                    ))}
-                </div>
+                                                </CardBody>
+                                            </Card>
+                                        </Col>
+                                    ))}
+                                </Row>
+                            </TabPanel>
+                        ))}
+                    </div>
+                </Container>
+                <Container>
+
+                </Container>
             </Container>
-            <Container>
-
-            </Container>
-        </Container>
-    );
+        );
+    }
 };
 
 Automation.propTypes = {
