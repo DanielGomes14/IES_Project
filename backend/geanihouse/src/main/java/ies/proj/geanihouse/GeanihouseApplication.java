b049ca9fc1b878623d74574d55e0ddc206d6d6e1
Daniel Gomes <dagomes@ua.pt>
2021-01-02 21:01:40 +0000

    Added API Docs

--- backend/geanihouse/src/main/java/ies/proj/geanihouse/GeanihouseApplication.java
+++ backend/geanihouse/src/main/java/ies/proj/geanihouse/GeanihouseApplication.java
@@ -4,10 +4,15 @@ import ies.proj.geanihouse.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableBinding(Sink.class)
@@ -18,5 +23,6 @@ public class GeanihouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeanihouseApplication.class, args);
 	}
+
 
 }
