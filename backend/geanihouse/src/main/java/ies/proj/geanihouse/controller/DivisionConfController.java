427106bd8ef24ff662b8209ea368bff4f5931f4e
BrunosBastos <Brunosb@ua.pt>
2021-01-02 19:35:06 +0000

    Added Put Mapping

--- backend/geanihouse/src/main/java/ies/proj/geanihouse/controller/DivisionConfController.java
+++ backend/geanihouse/src/main/java/ies/proj/geanihouse/controller/DivisionConfController.java
@@ -36,21 +36,22 @@ public class DivisionConfController {
     private TypeRepository typeRepository;
 
 
-    @GetMapping("/divisionconfigurations")
+    @GetMapping("/divisions/configurations")
     public List<DivisionConf> getAllDivisionConfs(){
         return  divisionConfRepository.findAll();
     }
-    @GetMapping("/divisions/{id}/divisionconfigurations")
+    @GetMapping("/divisions/{id}/configurations")
     public ResponseEntity<?> getDivisionConfs(@PathVariable(value = "id") long id) throws ResourceNotFoundException {
 
         Division division = divisionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException ("Could not find division with id :: " + id));
         LOG.info("Returning division Configurations for division with id  " + id);
-        List<DivisionConf> divisionConfs =divisionConfRepository.findAllByDivisionId(id);
+        List<DivisionConf> divisionConfs = divisionConfRepository.findAllByDivisionId(id);
         return  ResponseEntity.ok().body(divisionConfs);
+
     }
 
-    @PostMapping("/divisionconfigurations")
-    public  DivisionConf addDivisionConfs(@Valid @RequestBody DivisionConf divisionConf) throws  ResourceNotFoundException, ErrorDetails{
+    @PostMapping("/divisions/configurations")
+    public DivisionConf addDivisionConfs(@Valid @RequestBody DivisionConf divisionConf) throws  ResourceNotFoundException, ErrorDetails{
 
         //check if division exists
         Division division= divisionRepository.findById(divisionConf.getDivision().getId())
@@ -67,17 +68,17 @@ public class DivisionConfController {
 
 
         //check the contraints to create a new Configuration
-        String error_message= checkValuesContraints(divisionConf);
+        String error_message = checkValuesContraints(divisionConf);
 
-        if (error_message!=null) throw new ErrorDetails(error_message);
-        error_message=checkConfsTypes(divisionConf,division);
-        if(error_message!=null)throw new ErrorDetails(error_message);
+        if (error_message != null) throw new ErrorDetails(error_message);
+        error_message = checkConfsTypes(divisionConf,division);
+        if(error_message != null)throw new ErrorDetails(error_message);
 
         LOG.info("Success inserting new Configuration for division " + division.getId());
         return divisionConfRepository.save(divisionConf);
     }
 
-    @DeleteMapping("/divisionconfigurations/{id}")
+    @DeleteMapping("/divisions/configurations/{id}")
     public Map<String,Boolean> deleteDivisionConf(@PathVariable(value ="id") long id) throws ResourceNotFoundException{
         DivisionConf divisionConf = divisionConfRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Could not find division configuration with id ::" + id));
@@ -87,24 +88,56 @@ public class DivisionConfController {
         return response;
     }
 
+    @PutMapping("/divisions/configurations/{id}")
+    public ResponseEntity<?> updateDivisionConf(@PathVariable(value = "id") long id, @Valid @RequestBody DivisionConf dconf) throws ResourceNotFoundException{
+        DivisionConf divisionConf = divisionConfRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Could not find division configuration with id ::" + id));
+        
 
-    public  String checkValuesContraints(DivisionConf divisionConf){
+        divisionConf.setMinValue(dconf.getMinValue());
+        divisionConf.setMaxValue(dconf.getMaxValue());
+        divisionConfRepository.save(divisionConf);
+
+        return  ResponseEntity.ok().body(divisionConf);
+    }
+
+    private String checkValuesContraints(DivisionConf divisionConf){
         String error_message=null;
+        String type = divisionConf.getType().getName();
         if (divisionConf.getMinValue() > divisionConf.getMaxValue()){
-            error_message="Invalid Values, minimum value cannot be less than the max value!";
+            error_message = "Invalid Values, minimum value cannot be less than the max value!";
             LOG.warn(error_message);
         }
-        else if(divisionConf.getMaxValue()-divisionConf.getMinValue()<=1){
-            error_message="Invalid Values, the difference between the max and min value must be greater than 1!";
+        else if(divisionConf.getMaxValue() - divisionConf.getMinValue() <= 5){
+            error_message = "Invalid Values, the difference between the max and min value must be greater than 1!";
             LOG.warn(error_message);
         }
+
+        double minValue = divisionConf.getMinValue();
+        double maxValue = divisionConf.getMaxValue();
+
+        if (type.equals("Temperature")){
+            minValue = 15;
+            maxValue = 35;
+        }else if (type.equals("Humidity")){
+            minValue = 40;
+            maxValue = 60;
+        }else if (type.equals("Luminosity")){
+            minValue = 20;
+            maxValue = 80;
+        }
+
+        if ( minValue > divisionConf.getMinValue() || maxValue < divisionConf.getMaxValue())
+            error_message = "Invalid Values, "+type+" values are between "+ minValue+ "-"+maxValue;
+
+
         return error_message;
     }
 
-    public String checkConfsTypes(DivisionConf divisionConf,Division division){
+    private String checkConfsTypes(DivisionConf divisionConf,Division division){
         //verify if there's not a sensor of the type present on the configuration that we want to add
-        String error_message=null;
-        boolean valid=false;
+        String error_message = null;
+        boolean valid = false;
         for(Sensor s : division.getSensors()){
             if (s.getType().getName().equals(divisionConf.getType().getName())) {
                 valid = true;
@@ -118,13 +151,12 @@ public class DivisionConfController {
         }
         for(DivisionConf conf : division.getDivisionConf()){
             if(conf.getType().getName().equals(divisionConf.getType().getName())){
-                error_message= "Cannot have two configurations of same type in the division " + division.getId();
+                error_message = "Cannot have two configurations of same type in the division " + division.getId();
                 LOG.warn(error_message);
                 return error_message;
             }
         }
         return error_message;
     }
-
 }
 
