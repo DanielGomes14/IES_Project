a2e238c14b4af6928b6505d46c49b0aa0ff5959c
BrunosBastos <Brunosb@ua.pt>
2021-01-12 00:10:55 +0000

    profile page

--- projWebsite/src/components/user-profile/UserAccountDetails.js
+++ projWebsite/src/components/user-profile/UserAccountDetails.js
@@ -18,130 +18,128 @@ import {
 } from "shards-react";
 import UserDetails from "../user-profile-lite/UserDetails";
 
-const UserAccountDetails = ({ title,userDetails }) => (
-  <Card small className="mb-4">
-    <CardHeader className="border-bottom">
-      <h6 className="m-0">{title}</h6>
-    </CardHeader>
-    <ListGroup flush>
-      <ListGroupItem className="p-3">
-        <Row>
-          <Col>
-            <Form>
-              <Row form>
-                {/* First Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feFirstName">First Name</label>
-                  <FormInput
-                    id="feFirstName"
-                    placeholder="First Name"
-                    value={userDetails.fname}
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* Last Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feLastName">Last Name</label>
-                  <FormInput
-                    id="feLastName"
-                    placeholder="Last Name"
-                    value={userDetails.lname}
-                    onChange={() => {}}
-                  />
-                </Col>
-              </Row>
-              <Row form>
-                {/* Email */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feEmail">Email</label>
-                  <FormInput
-                    type="email"
-                    id="feEmail"
-                    placeholder="Email Address"
-                    value={userDetails.email}
-                    onChange={() => {}}
-                    autoComplete="email"
-                  />
-                </Col>
-                {/* Password */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="fePassword">Password</label>
-                  <FormInput
-                    type="password"
-                    id="fePassword"
-                    placeholder="Password"
-                    value={userDetails.password}
-                    onChange={() => {}}
-                    autoComplete="current-password"
-                  />
-                </Col>
-              </Row>
-              <Row form>
-                <Col md="12">
-                <label htmlFor="fePhoto">Profile Picture</label>
-              <InputGroup>
-              <FormInput value ={userDetails.avatar_name} placeholder="Enter here your profile pic" />
-              
-              <InputGroupAddon type="append">
-                <Button theme="primary">Upload Photo</Button>
-              </InputGroupAddon>
-            </InputGroup>
-            </Col>
-            </Row>
-              <Row style={{paddingTop:"15px"}} form>
-                {/* City */}
-                <Col md="3" className="form-group">
-                  <label htmlFor="feBirth">Birth</label>
-                  <FormInput
-                    id="feBirth"
-                    placeholder="Birth Date: dd/mm/yyyy"
-                    value={userDetails.birth}
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* State */}
-                <Col md="2" className="form-group">
-                  <label htmlFor="feInputState">Sex</label>
-                  <FormSelect id="feInputState">
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
-                  </FormSelect>
-                </Col>
-                {/* Zip Code */}
-              </Row>
-              <Button theme="accent">Update Account</Button>
-            </Form>
-          </Col>
-        </Row>
-      </ListGroupItem>
-    </ListGroup>
-  </Card>
-);
 
-UserAccountDetails.propTypes = {
-  /**
-   * The component's title.
-   */
-  title: PropTypes.string,
-  userDetails: PropTypes.object
-};
+class UserAccountDetails extends React.Component {
 
-UserAccountDetails.defaultProps = {
-  title: "Account Details",
-  userDetails: {
-    fname: "Antonio",
-    lname: "Silva",
-    avatar_name: "3.jpg",
-    avatar: require("./../../images/avatars/3.jpg" ),
-    metaTitle: "Description",
-    birth: "5/10/1985",
-    email:"antonio@example.com",
-    password:"examplepass",
-    metaValue:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eaque, quidem, commodi soluta qui quae minima obcaecati quod dolorum sint alias, possimus illum assumenda eligendi cumque?"
+
+  constructor(props){
+    super(props);
+    this.user = props.user;
   }
-  
-};
+
+  handleChange(event) {
+		const {name, value} = event.target;
+    this.setState({ [name]: value });
+	}
+
+
+  render() {
+
+    return (
+      <Card small className="mb-4">
+        <CardHeader className="border-bottom">
+          <h6 className="m-0">User Details</h6>
+        </CardHeader>
+        <ListGroup flush>
+          <ListGroupItem className="p-3">
+            <Row>
+              <Col>
+                <Form>
+                  <Row form>
+                    {/* First Name */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feFirstName">First Name</label>
+                      <FormInput
+                        id="feFirstName"
+                        placeholder="First Name"
+                        value={userDetails.fname}
+                        onChange={() => { }}
+                      />
+                    </Col>
+                    {/* Last Name */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feLastName">Last Name</label>
+                      <FormInput
+                        id="feLastName"
+                        placeholder="Last Name"
+                        value={userDetails.lname}
+                        onChange={() => { }}
+                      />
+                    </Col>
+                  </Row>
+                  <Row form>
+                    {/* Email */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feEmail">Email</label>
+                      <FormInput
+                        type="email"
+                        id="feEmail"
+                        placeholder="Email Address"
+                        value={userDetails.email}
+                        onChange={() => { }}
+                        autoComplete="email"
+                      />
+                    </Col>
+                    {/* Password */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="fePassword">Password</label>
+                      <FormInput
+                        type="password"
+                        id="fePassword"
+                        placeholder="Password"
+                        value={userDetails.password}
+                        onChange={() => { }}
+                        autoComplete="current-password"
+                      />
+                    </Col>
+                  </Row>
+                  <Row form>
+                    <Col md="12">
+                      <label htmlFor="fePhoto">Profile Picture</label>
+                      <InputGroup>
+                        <FormInput value={userDetails.avatar_name} placeholder="Enter here your profile pic" />
+
+                        <InputGroupAddon type="append">
+                          <Button theme="primary">Upload Photo</Button>
+                        </InputGroupAddon>
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <Row style={{ paddingTop: "15px" }} form>
+                    {/* City */}
+                    <Col md="3" className="form-group">
+                      <label htmlFor="feBirth">Birth</label>
+                      <FormInput
+                        id="feBirth"
+                        placeholder="Birth Date: dd/mm/yyyy"
+                        value={userDetails.birth}
+                        onChange={() => { }}
+                      />
+                    </Col>
+                    {/* State */}
+                    <Col md="2" className="form-group">
+                      <label htmlFor="feInputState">Sex</label>
+                      <FormSelect id="feInputState">
+                        <option>Male</option>
+                        <option>Female</option>
+                        <option>Other</option>
+                      </FormSelect>
+                    </Col>
+                    {/* Zip Code */}
+                  </Row>
+                  <Button theme="accent">Update Account</Button>
+                </Form>
+              </Col>
+            </Row>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+    )
+
+
+  }
+
+
+}
 
 export default UserAccountDetails;
--- projWebsite/src/components/user-profile/UserDetails.js
+++ projWebsite/src/components/user-profile/UserDetails.js
@@ -8,67 +8,62 @@ import {
   ListGroup,
   ListGroupItem,
 } from "shards-react";
+import ProfileService from "../../services/ProfileService";
 
 const calculate_age = (dob1) => {
   var today = new Date();
   var birthDate = new Date(dob1);  // create a date object directly from `dob1` argument
   var age_now = today.getFullYear() - birthDate.getFullYear();
   var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
-  {
-      age_now--;
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age_now--;
   }
   return age_now;
 
 }
 
+class UserDetails extends React.Component {
 
-const UserDetails = ({ userDetails }) => (
-  <Card small className="mb-4 pt-3">
-    <CardHeader className="border-bottom text-center">
-      <div className="mb-3 mx-auto">
-        <img
-          className="rounded-circle"
-          src={userDetails.avatar}
-          alt={userDetails.name}
-          width="110"
-        />
-      </div>
-      <h4 className="mb-0">{userDetails.name}</h4>
-      <span className="text-muted d-block mb-2">Age: {calculate_age(userDetails.birth)}</span>
-      <h5><Badge theme="info">
-        {userDetails.email}
-      </Badge></h5>
-    </CardHeader>
-    <ListGroup flush>
-      <ListGroupItem className="p-4">
-        <strong className="text-muted d-block mb-2">
-          Division Permissions:
-        </strong>
-        <span>Kitchen, Living Room, Bedroom #1</span>
-      </ListGroupItem>
-    </ListGroup>
-   
-  </Card>
-);
-
-UserDetails.propTypes = {
-  /**
-   * The user details object.
-   */
-  userDetails: PropTypes.object
-};
-
-UserDetails.defaultProps = {
-  userDetails: {
-    name: "Antonio Silva",
-    avatar: require("./../../images/avatars/3.jpg"),
-    metaTitle: "Description",
-    email:"antonio@example.com",
-    birth: "5/10/1985",
-    metaValue:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eaque, quidem, commodi soluta qui quae minima obcaecati quod dolorum sint alias, possimus illum assumenda eligendi cumque?"
+  constructor(props) {
+    super(props);
+    this.user = props.user;
+    console.log(this.user);
+    
   }
-};
+
+  
+
+  componentDidMount() {
+  }
+
+  render() {
+
+    if(!this.user)
+      return null;
+
+    return (
+      <Card small className="mb-4 pt-3">
+        <CardHeader className="border-bottom text-center">
+          <div className="mb-3 mx-auto">
+            <img
+              className="rounded-circle"
+              //src={this.user.client.profile_pic}
+              //alt={this.user.client.name}
+              width="110"
+            />
+          </div>
+          <h4 className="mb-0">{this.user.client.firstName} {this.user.client.lastName} </h4>
+          <span className="text-muted d-block mb-2">Age: {calculate_age(this.user.client.birth)}</span>
+          <h5><Badge theme="info">
+            {this.user.client.email}
+          </Badge></h5>
+        </CardHeader>
+      </Card>
+    )
+  }
+}
+
+
+
 
 export default UserDetails;
