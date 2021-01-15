68268a0e5f057b8e9a84c006bf241911097e08ae
BrunosBastos <Brunosb@ua.pt>
2021-01-15 16:36:11 +0000

    done removing data after 1 week

--- backend/geanihouse/src/main/java/ies/proj/geanihouse/config/DeleteSensorDataConfig.java
+++ backend/geanihouse/src/main/java/ies/proj/geanihouse/config/DeleteSensorDataConfig.java
@@ -1,8 +1,9 @@
-package ies.proj.geanihouse.service;
+package ies.proj.geanihouse.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Calendar;
@@ -14,18 +15,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
-class DeleteDataService{
+class DeleteSensorDataConfig{
     @Autowired
     private SensorDataRepository sensorDataRepository;
 
-    @Scheduled(cron = "*/10 * * * * *" ) //     0 0 1 * * MON
+    @Transactional
+    @Scheduled(cron = "0 0 1 * * MON" ) //      */20 * * * * *
     public void scheduleTaskUsingCronExpression() {
         // delete data 1 week old
         Calendar cal = Calendar.getInstance();
         Date today = cal.getTime();
-        //cal.add(Calendar.WEEK_OF_YEAR, -1);
-        cal.add(Calendar.SECOND, -10);
-        System.out.println(cal.getTime());
+        cal.setTime(today);
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        //cal.add(Calendar.SECOND, -20);
         Date expireDate = cal.getTime();
         //call the method
         sensorDataRepository.deleteByTimestampDateBefore(expireDate);
