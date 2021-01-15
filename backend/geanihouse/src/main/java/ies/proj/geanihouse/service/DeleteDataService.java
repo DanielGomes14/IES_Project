6683ea16ff00faf11ca26335e67f032dd571f42e
BrunosBastos <Brunosb@ua.pt>
2021-01-15 00:42:21 +0000

    Scheduler working but data is not being deleted

--- backend/geanihouse/src/main/java/ies/proj/geanihouse/service/DeleteDataService.java
+++ backend/geanihouse/src/main/java/ies/proj/geanihouse/service/DeleteDataService.java
@@ -3,25 +3,29 @@ package ies.proj.geanihouse.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.support.CronTrigger;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Calendar;
 import java.util.Date;
 import ies.proj.geanihouse.repository.SensorDataRepository;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@Service
+@Configuration
+@EnableScheduling
 class DeleteDataService{
     @Autowired
     private SensorDataRepository sensorDataRepository;
 
-    @Scheduled(cron = "/10 * * * * * ") //     0 0 1 * * MON
+    @Scheduled(cron = "*/10 * * * * *" ) //     0 0 1 * * MON
     public void scheduleTaskUsingCronExpression() {
         // delete data 1 week old
         Calendar cal = Calendar.getInstance();
         Date today = cal.getTime();
-        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        //cal.add(Calendar.WEEK_OF_YEAR, -1);
+        cal.add(Calendar.SECOND, -10);
+        System.out.println(cal.getTime());
         Date expireDate = cal.getTime();
         //call the method
         sensorDataRepository.deleteByTimestampDateBefore(expireDate);
